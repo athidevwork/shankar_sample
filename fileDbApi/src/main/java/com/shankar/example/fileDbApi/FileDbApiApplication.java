e91f6da44710ee8cb565f5154352a0926dcb4360
Athiyaman Muthukumarasamy <athi@Athiyamans-MBP.fios-router.home>
2020-10-16 21:18:01 -0400

    Added rest api initial revision

---
+++ fileDbApi/src/main/java/com/shankar/example/fileDbApi/FileDbApiApplication.java
@@ -0,0 +1,13 @@
+package com.shankar.example.fileDbApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileDbApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileDbApiApplication.class, args);
+	}
+
+}
---
+++ fileDbApi/src/test/java/com/shankar/example/fileDbApi/FileDbApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shankar.example.fileDbApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileDbApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
